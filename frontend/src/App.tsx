7483966a5b3029034a347ac9c8562a4946fc50ac
Jujunju <selamatgunakan@gmail.com>
2026-06-05 10:04:35 +0700

    konfigurasi: perbaikan di file App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react' 
-import LandingPage from './presentation/pages/Landingpage'
+import LandingPage from './presentation/pages/LandingPage'
 
 export default function App() {
   return (
